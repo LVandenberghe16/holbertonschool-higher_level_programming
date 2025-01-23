5fcc07e96fbda98e6d688f64343077bd29afe4ba
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-01-23 01:47:34 +0100

    oui

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module defines a function to add two integers.
+
+The function `add_integer` takes two arguments (a and b),
+and returns their sum, casting any floats to integers.
+"""
+
+def matrix_divided(matrix, div):
+    """
+    Adds two integers or floats, returning the result as an integer.
+
+    Parameters:
+    a (int, float): The first number.
+    b (int, float, optional): The second number (default is 98).
+
+    Returns:
+    int: The sum of the two numbers.
+
+    Raises:
+    TypeError: If a or b is not an integer or float.
+
+    Example:
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(100.5, 2)
+    102
+    >>> add_integer(2)
+    100
+    """
