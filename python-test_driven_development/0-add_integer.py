ccf09a60f573e7b24d02bd3859fa105a76dade2a
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-01-23 01:57:17 +0100

    oui

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -28,8 +28,8 @@ def add_integer(a, b=98):
     >>> add_integer(2)
     100
     """
-    if type(a) not in [int, float]:
+    if type(a) not in [int, float] or a != a:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if type(b) not in [int, float] or b!= b:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
