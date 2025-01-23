5f100f8eb97e3e28fad079780c32b30740ada1cd
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-01-23 13:47:23 +0100

    oui

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -29,8 +29,8 @@ def add_integer(a, b=98):
     >>> add_integer(2)
     100
     """
-    if type(a) not in [int, float] or a != a:
+    if type(a) not in [int, float] or a == "nan":
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float] or b != b:
+    if type(b) not in [int, float] or b == "nan":
         raise TypeError("b must be an integer")
     return int(a) + int(b)
