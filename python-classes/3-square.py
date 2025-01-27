efd925a80a39f41f58068490a03259f4aed118d9
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-01-27 17:08:34 +0100

    oui

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Ce module définit une classe square (ou carré)
+"""
+
+
+class Square:
+    """
+    Cette classe représente un carré
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
