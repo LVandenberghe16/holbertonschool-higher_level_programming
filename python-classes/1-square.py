97d2be6561a1448152f356e20ee2127583080eb8
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-01-27 16:52:02 +0100

    oui

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Ce module définit une classe square (ou carré)
+"""
+
+
+class Square:
+    """
+    Cette classe représente un carré
+    """
+
+    def __init__(self, size):
+        self.__size = size
