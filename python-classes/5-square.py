7de0636df8b72e38b7ea608a83b6587e94a2da70
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-01-27 17:30:37 +0100

    oui

---
+++ python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Ce module définit une classe square (ou carré)
+"""
+
+
+class Square:
+    """
+    Cette classe représente un carré
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Getter pour l'attribut __size.
+        Retourne la valeur de __size.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter pour l'attribut __size.
+        Vérifie que value est un entier >= 0 avant de l'affecter.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Returns the area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+                print()
+        else :
+            for _ in range (self.__size):
+                print("#" * self.__size)
