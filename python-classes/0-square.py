bc422aed4dc8ea93cfe68c99061b9549e27b5e2d
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-01-27 16:40:28 +0100

    oui

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""
+Ce module définit une classe square (ou carré)
+Pour l'instant ce module est vide
+Il faudra définir ce qu'est un carré
+"""
+
+
 class Square:
+    """
+    Cette classe représente un carré
+    Pour l'instant ce carré est vide
+    Il faudra définir le carré
+    """
+
     pass
