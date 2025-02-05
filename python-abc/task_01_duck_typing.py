cb2f699bd2a4923aa13081526c2c82caa4fbb7c4
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-02-06 00:26:46 +0100

    oui

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+
+"""
+Module
+"""
+
 from abc import ABC, abstractmethod
 from math import pi
 
-
 class Shape(ABC):
+    """An abstract class representing a shape."""
     @abstractmethod
     def area(self):
         pass
@@ -11,33 +16,33 @@ class Shape(ABC):
     @abstractmethod
     def perimeter(self):
         pass
-
 
 class Circle(Shape):
+    """A class representing a circle."""
     def __init__(self, radius):
-        self.radius = abs(radius)
+        self.__radius = abs(radius)
 
     def area(self):
-        return pi * (self.radius * self.radius)
+        return pi * self.__radius ** 2
 
     def perimeter(self):
-        return 2 * pi * self.radius
-
+        return 2 * pi * self.__radius
 
 class Rectangle(Shape):
+    """A class representing a rectangle."""
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     def area(self):
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
-        return (self.width + self.height) * 2
+        return 2 * (self.__width + self.__height)
 
-
-def shape_info(objet):
-    area = objet.area()
-    perimeter = objet.perimeter()
+def shape_info(shape):
+    """Prints the area and perimeter of a shape using ducktaping."""
+    area = shape.area()
+    perimeter = shape.perimeter()
     print(f"Area: {area}")
     print(f"Perimeter: {perimeter}")
