301d85a57ecf92c87b762bdce63e92b5670d3309
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-02-05 23:49:55 +0100

    oui

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -4,27 +4,19 @@
 Module
 """
 
-
 class VerboseList(list):
-    """Classe qui étend list en affichant des messages pour certaines opérations."""
-
     def append(self, item):
-        """Ajoute un élément à la liste avec un message."""
+        print("Added [{}] to the list".format(item))
         super().append(item)
-        print(f"Added [{item}] to the list.")
 
-    def extend(self, iterable):
-        """Ajoute plusieurs éléments à la liste avec un message."""
-        super().extend(iterable)
-        print(f"Extended the list with [{len(iterable)}] items.")
+    def pop(self, item):
+        print("Removed [{}] from the list".format(item))
+        super().pop(item)
+
+    def extend(self, item):
+        print("Extended the list with [{}]".format(item))
+        super().extend(item)
 
     def remove(self, item):
-        """Supprime un élément de la liste avec un message."""
-        print(f"Removed [{item}] from the list.")
-        super().remove(item)
-
-    def pop(self, index=-1):
-        """Retire et retourne un élément de la liste avec un message."""
-        item = super().pop(index)
-        print(f"Popped [{item}] from the list.")
-        return item
+        print("Removed [{}] from the list".format(item))
+        return super().remove(item)
