eb77f8817e90cef395b3431b7d66594570f8a45f
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-02-11 00:15:51 +0100

    oui

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,5 +5,5 @@ Module de json
 
 
 def class_to_json(obj):
-    """Returns the dictionary description for JSON serialization of an object"""
+    """Returns the dictionary description for JSON serialization of object"""
     return obj.__dict__
