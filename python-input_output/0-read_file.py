f1140784c56323120653c0a6cca6b2a1248735d8
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-02-10 15:45:16 +0100

    oui

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,9 @@ Module
 
 
 def read_file(filename=""):
+    """
+    méthode pour lire un fichier et le print
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         content = f.read()
         print(content, end="")
