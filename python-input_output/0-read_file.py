a085bdc1cd8656dca585b5e40c00c686900e1f4b
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-02-10 15:40:33 +0100

    oui

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    f = open(filename, 'r', encoding="utf-8")
-    print(f.read())
-    f.close()
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read())
+        f.close()
