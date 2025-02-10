392660b346076de0b941dd38b2d9101c77b82c73
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-02-10 16:00:44 +0100

    oui

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -9,5 +9,8 @@ def append_write(filename="", text=""):
     méthode pour ajouter du texte dans un fichier
     """
     with open(filename, 'w', encoding="utf-8") as f:
-        f.append(text)
+        if f != 0:
+            f.write(text)
+        else:
+            f.append(text)
         return len(text)
