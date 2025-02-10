22a2c624c7872ada8b0d06d2801904b9b1a3cd24
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-02-10 21:30:11 +0100

    oui

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    méthode pour ajouter du texte dans un fichier
+    """
+    return json.dumps(my_obj)
