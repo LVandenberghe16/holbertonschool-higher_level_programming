9b269eb580e395daa8ccd9bde9a16800137c3cc4
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-02-10 23:47:41 +0100

    oui

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -11,5 +11,5 @@ def load_from_json_file(filename):
     """
     méthode pour ajouter du texte dans un fichier
     """
-    with open(filename, 'w', encoding="utf-8") as file:
+    with open(filename, 'r', encoding="utf-8") as file:
         return json.loads(file)
