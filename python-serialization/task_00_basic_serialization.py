e99025f4f5ff8a7a50054125ab6eb2e42016ff03
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-02-11 01:00:07 +0100

    oui

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module
+"""
+
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, 'w') as f:
+        return f.write(json.dumps(data))
+
+def load_and_deserialize(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.loads(f.read())
