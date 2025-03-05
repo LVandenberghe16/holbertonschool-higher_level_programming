29a35c6e6903ccbc429cc086ad1344e30f6184f0
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-03-05 23:02:34 +0100

    oui

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines the City model"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
+
+class City(Base):
+    """City class mapped to cities table"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
